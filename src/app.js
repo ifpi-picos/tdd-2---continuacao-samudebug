c60e3a1b1c872fec41ae28d94e2cdf111062581c
Samuel Martins <samuel.ubuntu@gmail.com>
2023-06-19 17:05:08 -0300

    Testes

--- src/app.js
+++ src/app.js
@@ -6,19 +6,22 @@ const Tarefas = require('./tarefas');
 const tarefas = new Tarefas();
 
 app.post('/tarefas', (req, res) => {
-
+    tarefas.adicionar(req.body.descricao);
+    res.status(201).send('Tarefa adicionada com sucesso!');
 });
 
 app.delete('/tarefas/:indice', (req, res) => {
-
+    tarefas.remover(req.params.indice);
+    res.status(200).send('Tarefa removida com sucesso!');
 });
 
 app.get('/tarefas', (req, res) => {
-
+    res.status(200).send(tarefas.todas());
 });
 
 app.patch('/tarefas/:indice', (req, res) => {
-
+    tarefas.marcarConcluida(req.params.indice);
+    res.status(200).send('Tarefa marcada como concluída!')
 });
 
 module.exports = app;
